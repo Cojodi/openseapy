e23ead56fb4cddfb41b5b26a8f417b1c0f2d8a53
Dmitrij Vinokour <dmitrij.vinokour@cojodi.com>
2024-04-21 13:47:36 +0200

    feat(rate_limiter): own release locks method

--- openseapy/utils/rate_limiter.py
+++ openseapy/utils/rate_limiter.py
@@ -1,6 +1,8 @@
 import asyncio as aio
 from abc import ABC, abstractmethod
 
+from aioredis_semaphore import Semaphore
+
 
 class RateLimiter(ABC):
     @abstractmethod
@@ -28,10 +30,18 @@ class AsyncioRateLimiter(RateLimiter):
 
 class AsyncRedisRateLimiter(RateLimiter):
     def __init__(self, redis, semaphore_value, redis_namespace, sleep_time=0):
-        from aioredis_semaphore import Semaphore
+        self.redis_namespace = redis_namespace
+        self.redis = redis
+        self.sleep_time = sleep_time
 
         self.sema = Semaphore(redis, semaphore_value, redis_namespace)
-        self.sleep_time = sleep_time
+
+    async def release(self):
+        _, keys = await self.redis.scan()
+        for key in keys:
+            key = key.decode()
+            if self.redis_namespace in key:
+                await self.redis.delete(key)
 
     async def limit(self, coro):
         async with self.sema:
